80c83f5634b3420bf465222531238dcde8a04730
Carlos <carlos.huete@outlook.es>
2025-01-29 10:58:25 +0000

    añadido plantilla html

--- saludo.py
+++ saludo.py
@@ -1,2 +1,13 @@
-nombre = input("Por favor, ingresa tu nombre: ")
-print("Hola, " + nombre + "!")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    nombre = None
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+    return render_template("index.html", nombre=nombre)
+
+if __name__ == "__main__":
+    app.run(debug=True)
